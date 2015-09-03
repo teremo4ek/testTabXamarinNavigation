2b8fc8102ad046b7d75df385260c8e47aaf49d6a
Yury Bely <ybely@scnsoft.com>
2015-09-03 17:45:18 +0300

    initial commit

---
+++ tabtest/Pages/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+using tabtest.Pages;
+
+using Xamarin.Forms;
+
+namespace tabtest
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage ()
+		{
+			InitializeComponent ();
+		}
+
+	  private void Button_OnClicked(object sender, EventArgs e)
+	  {
+	    //throw new NotImplementedException();
+	    this.Navigation.PushAsync(new SecondPage());
+	  }
+	}
+}
+
---
+++ tabtest/Pages/SecondPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace tabtest.Pages
+{
+  public partial class SecondPage : TabbedPage
+  {
+    public SecondPage()
+    {
+      InitializeComponent();
+    }
+
+    private void Button_OnClicked(object sender, EventArgs e)
+    {
+      //throw new NotImplementedException();
+    }
+  }
+}
